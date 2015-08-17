d356828272561f1be6f633ba5e2df76d8a321c1d
Charlie Retzler <charlie.retzler@ingagepartners.com>
2015-08-17 17:21:35 -0400

    US135 - Fixed issues with origin not matching with (cr.net) preview pane and causing issues with SS.

--- app/core.run.js
+++ app/core.run.js
@@ -1,66 +1,82 @@
-﻿(function() {
-    'use strict';
-    angular.module('crossroads.core').run( AppRun );
+﻿(function () {
+  'use strict';
+  angular.module('crossroads.core').run(AppRun);
 
-    AppRun.$inject = ['Session',
-        '$rootScope',
-        'MESSAGES',
-        '$http',
-        '$log',
-        '$state',
-        '$timeout',
-        '$location',
-        '$cookies'];
+  AppRun.$inject = ['Session',
+    '$rootScope',
+    'MESSAGES',
+    '$http',
+    '$log',
+    '$state',
+    '$timeout',
+    '$location',
+    '$cookies',
+    '$document'];
 
-    function AppRun(Session, $rootScope, MESSAGES, $http, $log, $state, $timeout, $location, $cookies) {
-        $rootScope.MESSAGES = MESSAGES;
+  function AppRun(Session, $rootScope, MESSAGES, $http, $log, $state, $timeout, $location, $cookies, $document) {
+    $rootScope.MESSAGES = MESSAGES;
+    debugger;
+    setOriginForCmsPreviewPane($document);
 
-        function clearAndRedirect(event, toState,toParams) {
-            console.log($location.search());
+    function clearAndRedirect(event, toState, toParams) {
+      console.log($location.search());
 
-            Session.clear();
-            $rootScope.userid = null;
-            $rootScope.username = null;
-            Session.addRedirectRoute(toState.name, toParams);
-            event.preventDefault();
-            $state.go('login');
-        }
-
-        $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
-            if (toState.name === 'logout') {
-                if (fromState.data === undefined || !fromState.data.isProtected) {
-                    Session.addRedirectRoute(fromState.name, fromParams);
-                }
-                else {
-                    Session.addRedirectRoute('home', '');
-                }
-                return;
-           }
-	    
-           if (toState.data !== undefined && toState.data.preventRouteAuthentication) {
-             return;
-           }
-           if (Session.isActive()) {
-              $http({
-                method: 'GET',
-                url :__API_ENDPOINT__ + 'api/authenticated',
-                withCredentials: true,
-                headers: {
-                  'Authorization': $cookies.get('sessionId')
-                }}).success(function (user) {
-                    $rootScope.userid = user.userId;
-                    $rootScope.username = user.username;
-                    $rootScope.roles = user.roles;
-                }).error(function (e) {
-                    clearAndRedirect(event, toState, toParams);
-                });
-            } else if (toState.data !== undefined && toState.data.isProtected) {
-                clearAndRedirect(event, toState, toParams);
-            } else {
-                $rootScope.userid = null;
-                $rootScope.username = null;
-            }
-        //}
-        });
+      Session.clear();
+      $rootScope.userid = null;
+      $rootScope.username = null;
+      Session.addRedirectRoute(toState.name, toParams);
+      event.preventDefault();
+      $state.go('login');
     }
+
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+      if (toState.name === 'logout') {
+        if (fromState.data === undefined || !fromState.data.isProtected) {
+          Session.addRedirectRoute(fromState.name, fromParams);
+        }
+        else {
+          Session.addRedirectRoute('home', '');
+        }
+        return;
+      }
+
+      if (toState.data !== undefined && toState.data.preventRouteAuthentication) {
+        return;
+      }
+      if (Session.isActive()) {
+        $http({
+          method: 'GET',
+          url: __API_ENDPOINT__ + 'api/authenticated',
+          withCredentials: true,
+          headers: {
+            'Authorization': $cookies.get('sessionId')
+          }
+        }).success(function (user) {
+          $rootScope.userid = user.userId;
+          $rootScope.username = user.username;
+          $rootScope.roles = user.roles;
+        }).error(function (e) {
+          clearAndRedirect(event, toState, toParams);
+        });
+      } else if (toState.data !== undefined && toState.data.isProtected) {
+        clearAndRedirect(event, toState, toParams);
+      } else {
+        $rootScope.userid = null;
+        $rootScope.username = null;
+      }
+      //}
+    });
+
+    function setOriginForCmsPreviewPane($document) {
+      var document = $document[0];
+
+      // work-around for displaying cr.net inside preview pane for CMS
+      var parentDomain = 'crossroads.net';
+      if (document.domain.toLowerCase() === 'localhost') {
+        parentDomain = 'localhost';
+      }
+
+      document.domain = parentDomain;
+    }
+  }
 })();
