4e7ce7816624319846bff36a0bb5e832c9c4dd17
Matt <silbermm@gmail.com>
2015-09-14 14:37:13 -0400

    Release v0.0.40

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,18 @@
-var gulp = require("gulp");
-var watch = require("gulp-watch");
-var gutil = require("gulp-util");
-var webpack = require("webpack");
-var gulpWebpack = require("webpack-stream");
-var WebpackDevServer = require("webpack-dev-server");
-var webpackConfig = require("./webpack.config.js");
-var svgSprite = require("gulp-svg-sprite");
-var replace = require("gulp-replace");
-var rename = require("gulp-rename");
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var gutil = require('gulp-util');
+var webpack = require('webpack');
+var gulpWebpack = require('webpack-stream');
+var WebpackDevServer = require('webpack-dev-server');
+var webpackConfig = require('./webpack.config.js');
+var svgSprite = require('gulp-svg-sprite');
+var replace = require('gulp-replace');
+var rename = require('gulp-rename');
+var argv = require('yargs').argv;
+var npm = require('npm');
+var fs = require('fs');
+var bump = require('gulp-bump');
+var git = require('gulp-git');
 
 var browserSyncCompiles = 0;
 var browserSync = require('browser-sync').create();
@@ -24,7 +29,96 @@ gulp.task("build-dev", ["webpack:build-dev"], function() {
 });
 
 // Production build
-gulp.task("build", ["webpack:build"]);
+gulp.task('build', ['webpack:build']);
+
+gulp.task('bump', function () {
+  return gulp.src(['./package.json'])
+    .pipe(bump())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('tag', ['npmPublish'], function(callback){
+  var branch = argv.branch;
+  var pkg = require('./package.json');
+  var v = 'v' + pkg.version;
+  var message = 'Release ' + v;
+
+  if (!branch) {
+    var error = new Error('Branch is required as an argument --branch development');
+    return callback(error);
+  }
+
+  return gulp.src('./')
+    .pipe(git.commit(message))
+    .pipe(git.tag(v, message))
+    .pipe(git.push('origin', branch, '--tags'), function(err){
+      if(err) { throw err; } 
+    })
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('npmPublish', ['bump'], function(callback) {
+  var username = argv.username;
+  var password = argv.password;
+  var email = argv.email;
+  if (!username) {
+      var usernameError = new Error('Username is required as an argument --username exampleUsername');
+      return callback(usernameError);
+  }
+  if (!password) {
+      var passwordError = new Error('Password is required as an argument --password  examplepassword');
+      return callback(passwordError);
+  }
+  if (!email) {
+      var emailError = new Error('Email is required as an argument --email example@email.com');
+      return callback(emailError);
+  }
+
+  var uri = 'http://registry.npmjs.org/';
+  npm.load(null, function (loadError) {
+    if (loadError) {
+      return callback(loadError);
+    }
+    var auth = {
+      username: username,
+      password: password,
+      email: email,
+      alwaysAuth: true
+    };
+    var addUserParams = {
+      auth: auth
+    };
+    npm.registry.adduser(uri, addUserParams, function (addUserError, data, raw, res) {
+      if (addUserError) {
+        return callback(addUserError);
+      }
+      var metadata = require('./package.json');
+      metadata = JSON.parse(JSON.stringify(metadata));
+      npm.commands.pack([], function (packError) {
+        if (packError) {
+          return callback(packError);
+        }
+        var fileName = metadata.name + '-' + metadata.version + '.tgz';
+        var bodyPath = require.resolve('./' + fileName);
+        var body = fs.createReadStream(bodyPath);
+        var publishParams = {
+            metadata: metadata,
+            access: 'public',
+            body: body,
+            auth: auth
+        };
+        npm.registry.publish(uri, publishParams, function (publishError, resp) {
+          if (publishError) {
+            return callback(publishError);
+          }
+          console.log('Publish succesfull: ' + JSON.stringify(resp));
+          return callback();
+        });
+      });
+    });
+  }); 
+
+});
 
 // For convenience, an "alias" to webpack-dev-server
 gulp.task("start", ["webpack-dev-server"]);
