f2dba0cd2b7423d15b9ae0268803855a926b9721
Matt <silbermm@gmail.com>
2015-09-15 13:16:29 -0400

    Release v0.0.54-1

--- Gulpfile.js
+++ Gulpfile.js
@@ -61,7 +61,7 @@ gulp.task('tag', ['bump', 'npmPublish'], function(){
     /*.pipe(git.tag(v, message, {quiet: true}, function(err){*/
       //if(err) { throw err; } 
     /*}))*/
-    .pipe(git.push('origin', branch, {args: ' --tags'}, function(err){
+    .pipe(git.push('origin', branch, {}, function(err){
       if (err) { throw err; }  
     }))
     .pipe(gulp.dest('./'));
