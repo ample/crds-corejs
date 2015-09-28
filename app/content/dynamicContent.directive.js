bc5a15fa1c7bfc011c67f3a76da150a4ed2cde1d
jpc11623 <john.cleaver@ingagepartners.com>
2015-09-28 13:02:38 -0400

    Provided explanation for use of $$ function

--- app/content/dynamicContent.directive.js
+++ app/content/dynamicContent.directive.js
@@ -2,6 +2,9 @@ module.exports = DynamicContentDirective;
 
 DynamicContentDirective.$inject = ['$compile'];
 
+// use of html.$$unwrapTrustedValue() is explained here:
+//http://stackoverflow.com/questions/22572696/bind-new-html-to-controller-after-calling-trustashtml-angularjs
+// This is needed to allow scripts to be embedded in CMS pages for US857
 function DynamicContentDirective($compile) {
  return {
       restrict: 'A',
