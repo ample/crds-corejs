a76cb7bc33634557459f05ab91522c12c787df5f
Matt <silbermm@gmail.com>
2015-10-13 14:36:27 -0400

    US2397 Utility function for multi-select attributes

--- app/services/attributeType.service.js
+++ app/services/attributeType.service.js
@@ -7,15 +7,45 @@
 
   function AttributeType($resource, $cookies) {
     return {
-      AttributeTypes: function(params) {
-        return $resource(__API_ENDPOINT__ + 'api/AttributeType/:id',
+      AttributeTypes: AttributeTypes,
+      transformPersonMultiAttributes: transformPersonMultiAttributes
+    };
+
+    function AttributeTypes(params) {
+      return $resource(__API_ENDPOINT__ + 'api/AttributeType/:id',
           params,
           {
             get: { method:'GET', cache: true },
             query: { method:'GET', cache: true, isArray:true }
           }
         );
-      }
-    };
+    }
+
+    /**
+     * Takes an attributeTypeId, the list of attributes from a person,
+     * the list of attributes defined for the particular attributeType and
+     * a predicate to transform the attributes that belong to a person
+     *
+     * returns the transformed list of attributes that the person has defined
+     */
+    function transformPersonMultiAttributes(attributeTypeId,
+        contactAttributes,
+        attributeList,
+        successPredicate) {
+
+      var contactAttrs = _.filter(contactAttributes, function(attr) {
+        return attr.attributeTypeId === attributeTypeId;
+      });
+
+      _.forEach(attributeList, function(attr) {
+        _.forEach(contactAttrs, function(mine) {
+          if (mine.attributeId === attr.attributeId) {
+            successPredicate(attr);
+          }
+        });
+      });
+
+      return attributeList;
+    }
   }
 })();
