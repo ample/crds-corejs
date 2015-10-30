39089f4ed976ba3b832525247a66c7afb1028bba
IngageSandiR <sandi.ritter@ingagepartners.com>
2015-10-30 08:01:36 -0400

    US225

--- app/services/image.service.js
+++ app/services/image.service.js
@@ -10,6 +10,7 @@
       Image: $resource(__API_ENDPOINT__ + 'api/image/:id'),
       ProfileImage: $resource(__API_ENDPOINT__ + 'api/image/profile/:id'),
       ProfileImageBaseURL: __API_ENDPOINT__ + 'api/image/profile/',
+      ImageBaseURL: __API_ENDPOINT__ + 'api/image/',
       DefaultProfileImage: '//crossroads-media.imgix.net/images/avatar.svg'
     };
   }
