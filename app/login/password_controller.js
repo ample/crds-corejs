2cba349264c09a251c6fbb5a75487a08f22f6f2e
Matt Silbernagel <silbermm@gmail.com>
2015-11-12 12:34:19 -0500

    DE639 Validation
    * Removed onSubmit from validation
    * Added touch to validation
    * fixed linting errors

--- app/login/password_controller.js
+++ app/login/password_controller.js
@@ -1,53 +1,61 @@
 require('../services/password_service');
 
 (function() {
-    'use strict';
+  'use strict';
 
-    module.exports = PasswordController;
+  module.exports = PasswordController;
 
-    PasswordController.$inject = [
-        '$rootScope',
-        '$log',
-        '$state',
-        'PasswordService'];
+  PasswordController.$inject = [
+    '$rootScope',
+    '$log',
+    '$state',
+    'PasswordService'
+  ];
 
-    function PasswordController(
+  function PasswordController(
         $rootScope,
         $log,
         $state,
         PasswordService) {
 
-        $log.debug('Inside Password Controller');
+    $log.debug('Inside Password Controller');
 
-        var vm = this;
+    var vm = this;
+    vm.saving = false;
+    vm.resetRequest = resetRequest;
+    vm.validateEmail = validateEmail;
+
+    function resetRequest() {
+      vm.saving = true;
+      if (vm.forgotpasswordform.$valid) {
+        // don't send reset request if the email doesn't exist -- this is a little backwards
+        // because we're using an existing api call
+        PasswordService.EmailExists.get({ email: vm.emailAddress }, function(response) {
+          $rootScope.$emit('notify', $rootScope.MESSAGES.generalError);
+          vm.saving = false;
+        },
+
+        function(error) {
+          var email = { email: vm.emailAddress };
+          PasswordService.ResetRequest.save(email).$promise.then(function(response) {
+            $rootScope.$emit('notify', $rootScope.MESSAGES.resetRequestSuccess);
+            $state.go('content', {link: '/'});
+          },
+
+          function(error) {
+            $rootScope.$emit('notify', $rootScope.MESSAGES.generalError);
+            vm.saving = false;
+          });
+        });
+      }
+      else {
         vm.saving = false;
+      }
+    }
 
-        vm.resetRequest = function(form) {
-
-            vm.saving = true;
-
-            if (form.forgotpasswordform.$valid) {
-
-                // don't send reset request if the email doesn't exist -- this is a little backwards
-                // because we're using an existing api call
-                PasswordService.EmailExists.get({ email: vm.emailAddress }, function (response) {
-                    $rootScope.$emit('notify', $rootScope.MESSAGES.generalError);
-                    vm.saving = false;
-                }, function (error) {
-                    var email = { 'email' : vm.emailAddress }
-                    PasswordService.ResetRequest.save(email).$promise.then(function (response) {
-                        $rootScope.$emit('notify', $rootScope.MESSAGES.resetRequestSuccess);
-                        $state.go('content', {link: '/'});
-                    }, function (error) {
-                        $rootScope.$emit('notify', $rootScope.MESSAGES.generalError);
-                        vm.saving = false;
-                    });
-                });
-            }
-            else {
-                vm.saving = false;
-            }
-        }
-
-    };
+    function validateEmail() {
+      return (vm.forgotpasswordform.emailfield.$error.email || vm.forgotpasswordform.$invalid) &&
+       vm.forgotpasswordform.emailfield.$touched;
+    }
+  }
 })();
