852bb1a52898e3cadb888d416fbddd00a220085d
jpc11623 <john.cleaver@ingagepartners.com>
2015-12-18 10:17:12 -0500

    Reset Password
    US2663 - Final commit of code

--- app/login/index.js
+++ app/login/index.js
@@ -4,6 +4,7 @@
   angular.module('crossroads.core')
     .controller('LoginController', require('./login_controller'))
     .controller('PasswordController', require('./password_controller'))
+      .controller('ResetPasswordController', require('./reset_password_controller'))
     .directive('loginForm', require('./login_form_directive'))
   ;
 
---
+++ app/login/reset_password_controller.js
@@ -0,0 +1,85 @@
+require('../services/password_service');
+require('../services/auth_service');
+
+(function() {
+  'use strict';
+
+  module.exports = ResetPasswordController;
+
+  ResetPasswordController.$inject = [
+      '$rootScope',
+      '$stateParams',
+      '$log',
+      '$state',
+      'PasswordService',
+      'TokenStatus'
+  ];
+
+  function ResetPasswordController(
+      $rootScope,
+      $stateParams,
+      $log,
+      $state,
+      PasswordService,
+      TokenStatus) {
+
+    $log.debug('Inside Password Controller');
+
+    var vm = this;
+    vm.saving = false;
+    vm.password = '';
+    vm.resetToken = $stateParams.token;
+    vm.pwprocessing = pwProcess;
+    vm.submitPasswordReset = submitPasswordReset;
+    vm.TokenStatus = TokenStatus;
+
+    activate();
+
+    function activate() {
+
+      vm.TokenStatus.$promise.then(function(resolve) {
+
+        var valueItem = resolve.TokenValid;
+        debugger;
+        if (valueItem === false) {
+          $rootScope.$emit('notify', $rootScope.MESSAGES.invalidPasswordResetKey);
+          $state.go('content', {link: '/'}); // redirect to home if invalid link
+        }
+      }), function(err) {
+
+        debugger;
+      };
+
+    };
+
+    function submitPasswordReset() {
+
+      if (vm.resetPasswordForm.$valid) {
+        vm.saving = true;
+
+        var reset = { password: vm.password, token: $stateParams.token };
+
+        PasswordService.ResetPassword.save(reset).$promise.then(function(response) {
+          $rootScope.$emit('notify', $rootScope.MESSAGES.resetRequestSuccess);
+          $state.go('login');
+        }, function(error) {
+
+          $rootScope.$emit('notify', $rootScope.MESSAGES.generalError);
+          vm.saving = false;
+        });
+      } else {
+        vm.saving = false;
+      }
+    }
+
+    function pwProcess() {
+      if ($scope.pwprocessing == 'SHOW') {
+        $scope.pwprocessing = 'HIDE';
+        $scope.inputType = 'text';
+      }      else {
+        $scope.pwprocessing = 'SHOW';
+        $scope.inputType = 'password';
+      }
+    };
+  }
+})();
